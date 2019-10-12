0f272c141fc2d145f09bfdbc0c6685c461a48425
Rafał <rafass1000@gmail.com>
2019-10-12 14:38:20 +0200

    asc

--- app/static/app/master.js
+++ app/static/app/master.js
@@ -1,1 +1,20 @@
-console.log("dupa")
+var services = {
+    navigation : function(){
+        return {
+            page : function(id){
+                var pagear = document.querySelectorAll('.page');
+                pagear = Array.prototype.slice.call(pagear);
+                pagear.forEach(e =>{
+                    try{
+                        e.style.display = none;
+                    }catch(Ee){
+
+                    }
+                })
+                document.querySelector(".page"+id).style.display = "block";
+            }
+        }
+    }
+}
+
+services.navigation().page(1);
