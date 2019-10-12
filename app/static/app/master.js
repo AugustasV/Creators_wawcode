d450e69fa3c5c3df9cd0b814e5efea33e4e9a85d
Rafał <rafass1000@gmail.com>
2019-10-12 19:08:45 +0200

    Front

--- app/static/app/master.js
+++ app/static/app/master.js
@@ -1,20 +1,3 @@
-var services = {
-    navigation : function(){
-        return {
-            page : function(id){
-                var pagear = document.querySelectorAll('.page');
-                pagear = Array.prototype.slice.call(pagear);
-                pagear.forEach(e =>{
-                    try{
-                        e.style.display = none;
-                    }catch(Ee){
-
-                    }
-                })
-                document.querySelector(".page"+id).style.display = "block";
-            }
-        }
-    }
-}
-
-services.navigation().page(1);
+document.querySelectorAll('.page').forEach(el =>{
+    el.style.display = none;
+})
